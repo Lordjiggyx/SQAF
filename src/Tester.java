a557862a9bcda14f6bf09586f13ca408afec5a00
Tomi Ilori <tomiilori123@gmail.com>
2020-05-10 16:52:24 +0100

    TDD- Test created to check if questions are being added to survey
    Survey class not refactored yet

--- src/Tester.java
+++ src/Tester.java
@@ -45,4 +45,19 @@ public class Tester {
 		Survey s = new Survey("My Questions" ,questions);
 		assertTrue("Should be an object of type arralylist",s.getQuestions() instanceof ArrayList);
 	}
+	
+	//Test to check that questions are being added to the survey
+	@Test
+	public void addQuestion()
+	{
+		//Creation of question object
+		Question one = new Question("Customer Service");
+		Question two = new Question("Food Quality");
+		
+		Survey s = new Survey("My Questions");
+		s.add(one);
+		s.add(two)
+		
+		assertEquals("Length of questions array should be 2",2 ,s.getQuestions().size());
+	}
 }
