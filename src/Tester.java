775c744e74a2eed9bf1089b059e752cd8e510828
Tomi Ilori <tomiilori123@gmail.com>
2020-05-14 13:20:20 +0100

    TDD - Minimum & Maximum Score for question
    This commit shows the controller being able to get the maximum and
    minimum score for a question in a survey and the corresponding tests

--- src/Tester.java
+++ src/Tester.java
@@ -106,124 +106,15 @@ public class Tester {
 	}
 	
 	@Test
-	public void maxQuestion()
+	public int maxQuestion(Survey s , int index)
 	{
-		
-		//Creation od questions and setting answer values
-		Question one = new Question("Customer Service");
-		one.setAnswer(3);
-		Question two = new Question("Food Quality");
-		two.setAnswer(2);
-		
-		//creation of a survey object and adding the questions to this survey
-		Survey s = new Survey();
-		s.add(one);
-		s.add(two);
-
-
-		
-		
-		//Simiulating a different set of answers for the same questions in the survey
-		ArrayList<Question> questions1 = new ArrayList<Question>();
-		//Keeps the names of the questions in the survey 
-		for(Question q :s.getQuestions() )
-		{
-			//new question with name of questions from the survey
-			Question nq = new Question(q.getQuestion());
-			//mimics a change in answer value
-			nq.setAnswer(2);
-			//added to a list to represent the questions with different value answers
-			questions1.add(nq);
-		}
-		
-		//Same process
-		ArrayList<Question> questions2 = new ArrayList<Question>();
-		for(Question q :s.getQuestions() )
-		{
-			Question nq = new Question(q.getQuestion());
-			nq.setAnswer(1);
-			questions2.add(nq);
-		}
-
-
-		//creating a survey response object that takes in the surveys questions and questions with different answers
-		surveyResponse sr = new surveyResponse(s.getQuestions());
-		surveyResponse sr1 = new surveyResponse(questions1);
-		surveyResponse sr2 = new surveyResponse(questions2);
-
-		//adding the survey response to the objects
-		s.addResponse(sr);
-		s.addResponse(sr1);
-		s.addResponse(sr2);
-		
-		//Feel free to uncomment and check the responses for different surevy responses
-//		System.out.println(s.getResponses().get(0).getResponses());
-//		System.out.println(s.getResponses().get(1).getResponses());
-//		System.out.println(s.getResponses().get(2).getResponses());
-
-		
-		assertEquals("max repsonse for question 1 should be 3", 3 , s.getMaxiumQuestion(0));
+		return s.getMaxiumQuestion(index);
 		
 	}
 	
 	@Test
-	public void minQuestion()
+	public int minQuestion(Survey s , int index)
 	{
-		
-		//Creation od questions and setting answer values
-		Question one = new Question("Customer Service");
-		one.setAnswer(1);
-		Question two = new Question("Food Quality");
-		two.setAnswer(2);
-		
-		//creation of a survey object and adding the questions to this survey
-		Survey s = new Survey();
-		s.add(one);
-		s.add(two);
-
-
-		
-		
-		//Simiulating a different set of answers for the same questions in the survey
-		ArrayList<Question> questions1 = new ArrayList<Question>();
-		//Keeps the names of the questions in the survey 
-		for(Question q :s.getQuestions() )
-		{
-			//new question with name of questions from the survey
-			Question nq = new Question(q.getQuestion());
-			//mimics a change in answer value
-			nq.setAnswer(5);
-			//added to a list to represent the questions with different value answers
-			questions1.add(nq);
-		}
-		
-		//Same process
-		ArrayList<Question> questions2 = new ArrayList<Question>();
-		for(Question q :s.getQuestions() )
-		{
-			Question nq = new Question(q.getQuestion());
-			nq.setAnswer(3);
-			questions2.add(nq);
-		}
-
-
-		//creating a survey response object that takes in the surveys questions and questions with different answers
-		surveyResponse sr = new surveyResponse(s.getQuestions());
-		surveyResponse sr1 = new surveyResponse(questions1);
-		surveyResponse sr2 = new surveyResponse(questions2);
-
-		//adding the survey response to the objects
-		s.addResponse(sr);
-		s.addResponse(sr1);
-		s.addResponse(sr2);
-		
-		//Feel free to uncomment and check the responses for different surevy responses
-//		System.out.println(s.getResponses().get(0).getResponses());
-//		System.out.println(s.getResponses().get(1).getResponses());
-//		System.out.println(s.getResponses().get(2).getResponses());
-
-		
-		assertEquals("min repsonse for question 1 should be 1", 1 , s.getMinimumQuestion(0));
-		
+		return s.getMinimumQuestion(index);
 	}
 }
