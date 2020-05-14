12a30bb1d08d547ba527c60b0f8e23069d4f29ac
Tomi Ilori <tomiilori123@gmail.com>
2020-05-14 13:14:16 +0100

    TDD - Standard and Average deviation of question
    This commit shows the controller being able to get the standard and
    average deviation of a question and the corresponding tests

--- src/ControllerTestSuite.java
+++ src/ControllerTestSuite.java
@@ -159,5 +159,181 @@ public class ControllerTestSuite {
 		
 	}
 	
+	@Test
+	public void MaximumScoreSurvey()
+	{
+		//Creation of question objectS
+		Question one = new Question("Customer Service");
+		one.setAnswer(3);
+		Question two = new Question("Food Quality");
+		two.setAnswer(1);
+		Question three = new Question("Food Quality");
+		three.setAnswer(3);
+		Question four = new Question("Food Quality");
+		four.setAnswer(4);
+		Question five = new Question("Food Quality");
+		five.setAnswer(5);
 	
+		
+
+		//Creation of survey class with just name
+		Survey s = new Survey("My Questions");
+		//adding questions to the surevy
+		s.add(one);
+		s.add(two);
+		s.add(three);
+		s.add(four);
+		s.add(five);
+
+		
+		assertEquals("maximum value should be 5", 5 , test.maxSurvey(s));
+	}
+	
+	@Test
+	public void MinimumScoreSurvey()
+	{
+		//Creation of question objectS
+		Question one = new Question("Customer Service");
+		one.setAnswer(3);
+		Question two = new Question("Food Quality");
+		two.setAnswer(1);
+		Question three = new Question("Food Quality");
+		three.setAnswer(3);
+		Question four = new Question("Food Quality");
+		four.setAnswer(4);
+		Question five = new Question("Food Quality");
+		five.setAnswer(5);
+	
+		
+
+		//Creation of survey class with just name
+		Survey s = new Survey("My Questions");
+		//adding questions to the surevy
+		s.add(one);
+		s.add(two);
+		s.add(three);
+		s.add(four);
+		s.add(five);
+
+		
+		assertEquals("minimum value should be 1", 1 , s.getMinimum());
+	}
+
+	@Test
+	public void averageDeviationQuestion()
+	{
+		
+		//Creation od questions and setting answer values
+		Question one = new Question("Customer Service");
+		one.setAnswer(3);
+		Question two = new Question("Food Quality");
+		two.setAnswer(4);
+		
+		//creation of a survey object and adding the questions to this survey
+		Survey s = new Survey();
+		s.add(one);
+		s.add(two);
+		
+		//Simiulating a different set of answers for the same questions in the survey
+		ArrayList<Question> questions1 = new ArrayList<Question>();
+		//Keeps the names of the questions in the survey 
+		for(Question q :s.getQuestions() )
+		{
+			//new question with name of questions from the survey
+			Question nq = new Question(q.getQuestion());
+			//mimics a change in answer value
+			nq.setAnswer(5);
+			//added to a list to represent the questions with different value answers
+			questions1.add(nq);
+		}
+		
+		//Same process
+		ArrayList<Question> questions2 = new ArrayList<Question>();
+		for(Question q :s.getQuestions() )
+		{
+			Question nq = new Question(q.getQuestion());
+			nq.setAnswer(3);
+			questions2.add(nq);
+		}
+
+
+		//creating a survey response object that takes in the surveys questions and questions with different answers
+		surveyResponse sr = new surveyResponse(s.getQuestions());
+		surveyResponse sr1 = new surveyResponse(questions1);
+		surveyResponse sr2 = new surveyResponse(questions2);
+
+		//adding the survey response to the objects
+		s.addResponse(sr);
+		s.addResponse(sr1);
+		s.addResponse(sr2);
+		
+		//Feel free to uncomment and check the responses for different surevy responses
+		//Use this link to check if the calculation is correct https://miniwebtool.com/average-deviation-calculator/
+//		System.out.println(s.getResponses().get(0).getResponses());
+//		System.out.println(s.getResponses().get(1).getResponses());
+//		System.out.println(s.getResponses().get(2).getResponses());
+		
+		assertEquals("average Deviation should be 0.89", 0.89 , test.aveDevQuestion(s, 0) ,1e-15);
+
+	}
+	
+	@Test
+	public void StandardDeviationQuestion()
+	{
+		//Creation od questions and setting answer values
+				Question one = new Question("Customer Service");
+				one.setAnswer(3);
+				Question two = new Question("Food Quality");
+				two.setAnswer(4);
+				
+				//creation of a survey object and adding the questions to this survey
+				Survey s = new Survey();
+				s.add(one);
+				s.add(two);
+
+
+				
+				
+				//Simiulating a different set of answers for the same questions in the survey
+				ArrayList<Question> questions1 = new ArrayList<Question>();
+				//Keeps the names of the questions in the survey 
+				for(Question q :s.getQuestions() )
+				{
+					//new question with name of questions from the survey
+					Question nq = new Question(q.getQuestion());
+					//mimics a change in answer value
+					nq.setAnswer(5);
+					//added to a list to represent the questions with different value answers
+					questions1.add(nq);
+				}
+				
+				//Same process
+				ArrayList<Question> questions2 = new ArrayList<Question>();
+				for(Question q :s.getQuestions() )
+				{
+					Question nq = new Question(q.getQuestion());
+					nq.setAnswer(3);
+					questions2.add(nq);
+				}
+
+
+				//creating a survey response object that takes in the surveys questions and questions with different answers
+				surveyResponse sr = new surveyResponse(s.getQuestions());
+				surveyResponse sr1 = new surveyResponse(questions1);
+				surveyResponse sr2 = new surveyResponse(questions2);
+
+				//adding the survey response to the objects
+				s.addResponse(sr);
+				s.addResponse(sr1);
+				s.addResponse(sr2);
+				
+				//Feel free to uncomment and check the responses for different surevy responses
+				//Use this link to check if the calculation is correct https://www.mathsisfun.com/data/standard-deviation-calculator.html
+//				System.out.println(s.getResponses().get(0).getResponses());
+//				System.out.println(s.getResponses().get(1).getResponses());
+//				System.out.println(s.getResponses().get(2).getResponses());
+
+				
+				assertEquals("standard Deviation should be 0.82", 0.82 , test.stanDevQuestion(s, 1) ,1e-15);
+	}
 }
--- src/Tester.java
+++ src/Tester.java
@@ -27,22 +27,8 @@ public class Tester {
 		return s;
 	}
 
-	//Test to see if collection of questions exists within survey object
-	@Test
-	public void surveyQuestions()
-	{		
-		//Creation of question object
-		Question one = new Question("Customer Service");
-		//Collection to represent questions
-		ArrayList<Question> questions = new ArrayList<Question>();
-		//adding question to list
-		questions.add(one);
 
-		Survey s = new Survey("My Questions" ,questions);
-		assertTrue("Should be an object of type arralylist",s.getQuestions() instanceof ArrayList);
-	}
-
-	//check to see that item has been added and survey has a list returns length of the arraylist
+	//add question to survey
 	public int addQuestion(Question q , Survey surv)
 	{
 		//Creation of question objectS
@@ -56,6 +42,7 @@ public class Tester {
 
 	}
 
+	//create survey response
 	public surveyResponse createSurveyResponse(Survey surv)
 	{		
 		//Creation of a new survey response object
@@ -63,7 +50,7 @@ public class Tester {
 		return sr;
 	}
 	
-
+	//set question answer
 	public int questionAnswer(Question q)
 	{
 		//Creation of question objwct
@@ -73,9 +60,7 @@ public class Tester {
 		return one.getAnswer();
 	}
 	
-
-	
-	
+	//getting the responses of a survey response
 	public ArrayList<Integer> surveyResponse(Survey s)
 		{
 			//Get the first survey response and the corresponding answers in the object
@@ -84,202 +69,40 @@ public class Tester {
 
 		}
 
-	//Test to get average deviation of a survey
-	@Test
+	//get average deviation of survey
 	public double aveDev(Survey s)
 	{		
 		return s.getAverageDeviation();
 	}
 	
-	//Test for the standard deviation
-	@Test
+	//get standard deviation of survey
 	public double standardDeviation(Survey s)
 	{
 		return s.getStandardDeviation();
 	}
 	
-	//Test for the max score in a survey
-	@Test
-	public void maxSurvey()
+	//get maximum value of survey
+	public int maxSurvey(Survey s)
 	{
-		//Creation of question objectS
-				Question one = new Question("Customer Service");
-				one.setAnswer(3);
-				Question two = new Question("Food Quality");
-				two.setAnswer(1);
-				Question three = new Question("Food Quality");
-				three.setAnswer(3);
-				Question four = new Question("Food Quality");
-				four.setAnswer(4);
-				Question five = new Question("Food Quality");
-				five.setAnswer(5);
-			
-				
-
-				//Creation of survey class with just name
-				Survey s = new Survey("My Questions");
-				//adding questions to the surevy
-				s.add(one);
-				s.add(two);
-				s.add(three);
-				s.add(four);
-				s.add(five);
-
-				//Have to pass in delta which is the maximum delta between expected and actual for which both numbers are still considered equal.
-				assertEquals("maximum value should be 5", 5 , s.getMaxium());
+		return s.getMaxium();
 	}
 	
-	//Test for the min score in a survey
-	@Test
-	public void minSurvey()
+	//get minimum value of a survey
+	public int minSurvey(Survey s)
 	{
-		//Creation of question objectS
-				Question one = new Question("Customer Service");
-				one.setAnswer(3);
-				Question two = new Question("Food Quality");
-				two.setAnswer(3);
-				Question three = new Question("Food Quality");
-				three.setAnswer(3);
-				Question four = new Question("Food Quality");
-				four.setAnswer(4);
-				Question five = new Question("Food Quality");
-				five.setAnswer(5);
-			
-				
-
-				//Creation of survey class with just name
-				Survey s = new Survey("My Questions");
-				//adding questions to the surevy
-				s.add(one);
-				s.add(two);
-				s.add(three);
-				s.add(four);
-				s.add(five);
-
-				assertEquals("maximum value should be 3", 3 , s.getMinimum());
+		return s.getMinimum();
 	}
 	
-	@Test
-	public void aveDevQuestion()
+	//get average deviation of a certain question in the survey
+	public double aveDevQuestion(Survey s , int index)
 	{
-		
-		//Creation od questions and setting answer values
-		Question one = new Question("Customer Service");
-		one.setAnswer(3);
-		Question two = new Question("Food Quality");
-		two.setAnswer(4);
-		
-		//creation of a survey object and adding the questions to this survey
-		Survey s = new Survey();
-		s.add(one);
-		s.add(two);
-
-
-		
-		
-		//Simiulating a different set of answers for the same questions in the survey
-		ArrayList<Question> questions1 = new ArrayList<Question>();
-		//Keeps the names of the questions in the survey 
-		for(Question q :s.getQuestions() )
-		{
-			//new question with name of questions from the survey
-			Question nq = new Question(q.getQuestion());
-			//mimics a change in answer value
-			nq.setAnswer(5);
-			//added to a list to represent the questions with different value answers
-			questions1.add(nq);
-		}
-		
-		//Same process
-		ArrayList<Question> questions2 = new ArrayList<Question>();
-		for(Question q :s.getQuestions() )
-		{
-			Question nq = new Question(q.getQuestion());
-			nq.setAnswer(3);
-			questions2.add(nq);
-		}
-
-
-		//creating a survey response object that takes in the surveys questions and questions with different answers
-		surveyResponse sr = new surveyResponse(s.getQuestions());
-		surveyResponse sr1 = new surveyResponse(questions1);
-		surveyResponse sr2 = new surveyResponse(questions2);
-
-		//adding the survey response to the objects
-		s.addResponse(sr);
-		s.addResponse(sr1);
-		s.addResponse(sr2);
-		
-		//Feel free to uncomment and check the responses for different surevy responses
-		//Use this link to check if the calculation is correct https://miniwebtool.com/average-deviation-calculator/
-//		System.out.println(s.getResponses().get(0).getResponses());
-//		System.out.println(s.getResponses().get(1).getResponses());
-//		System.out.println(s.getResponses().get(2).getResponses());
-
-		assertEquals("average Deviation should be 0.89", 0.89 , s.averageDeviationQuestion(0) ,1e-15);
-		
+			return s.averageDeviationQuestion(index);
 	}
 	
-	@Test
-	public void stanDevQuestion()
+	//get standard deviation of a certain question in the survey
+	public double stanDevQuestion(Survey s , int index)
 	{
-		
-		//Creation od questions and setting answer values
-		Question one = new Question("Customer Service");
-		one.setAnswer(3);
-		Question two = new Question("Food Quality");
-		two.setAnswer(4);
-		
-		//creation of a survey object and adding the questions to this survey
-		Survey s = new Survey();
-		s.add(one);
-		s.add(two);
-
-
-		
-		
-		//Simiulating a different set of answers for the same questions in the survey
-		ArrayList<Question> questions1 = new ArrayList<Question>();
-		//Keeps the names of the questions in the survey 
-		for(Question q :s.getQuestions() )
-		{
-			//new question with name of questions from the survey
-			Question nq = new Question(q.getQuestion());
-			//mimics a change in answer value
-			nq.setAnswer(5);
-			//added to a list to represent the questions with different value answers
-			questions1.add(nq);
-		}
-		
-		//Same process
-		ArrayList<Question> questions2 = new ArrayList<Question>();
-		for(Question q :s.getQuestions() )
-		{
-			Question nq = new Question(q.getQuestion());
-			nq.setAnswer(3);
-			questions2.add(nq);
-		}
-
-
-		//creating a survey response object that takes in the surveys questions and questions with different answers
-		surveyResponse sr = new surveyResponse(s.getQuestions());
-		surveyResponse sr1 = new surveyResponse(questions1);
-		surveyResponse sr2 = new surveyResponse(questions2);
-
-		//adding the survey response to the objects
-		s.addResponse(sr);
-		s.addResponse(sr1);
-		s.addResponse(sr2);
-		
-		//Feel free to uncomment and check the responses for different surevy responses
-		//Use this link to check if the calculation is correct https://www.mathsisfun.com/data/standard-deviation-calculator.html
-//		System.out.println(s.getResponses().get(0).getResponses());
-//		System.out.println(s.getResponses().get(1).getResponses());
-//		System.out.println(s.getResponses().get(2).getResponses());
-
-		
-		assertEquals("standard Deviation should be 0.82", 0.82 , s.StandardDeviationQuestion(1) ,1e-15);
-		
+		return s.StandardDeviationQuestion(index);
 	}
 	
 	@Test
