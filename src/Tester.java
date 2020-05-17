62904b97a16acd56123852efaeb684a8cddd0019
Tomi Ilori <tomiilori123@gmail.com>
2020-05-17 13:46:32 +0100

    TDD - Controller sum of responses
    The controller can return the sum of a survey reponse
    
    Also the test that prove this functionality works

--- src/Tester.java
+++ src/Tester.java
@@ -149,28 +149,9 @@ public class Tester {
 	}
 	
 	@Test
-	public void sumOfResponses()
+	public int sumOfResponses(surveyResponse sr)
 	{
-		//make survey and questions
-		Survey s = new Survey();
-		Question one = new Question("Customer Service"); 
-		one.setAnswer(4);
-		Question two = new Question("Food Quality");
-		two.setAnswer(5);
-		Question three = new Question("Food Quality");
-		three.setAnswer(2);
-		Question four = new Question("Food Quality");
-		four.setAnswer(2);
-		//Questions added to survey
-		s.add(one);
-		s.add(two);
-		s.add(three);
-		s.add(four);
-		
-		//survey response created with questions from survey
-		surveyResponse sr = new surveyResponse(s.getQuestions());
-		//Test to see that 4+5+2+2 = 13 PASS
-		assertEquals("Sum of survet repsonse should be 13" ,13 , sr.getResponsesSum());
+		return sr.getResponsesSum();
 	}
 
 	@Test
